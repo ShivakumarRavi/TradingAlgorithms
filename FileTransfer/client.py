c059a719d9be02d48788c3c66661ac0e9a6e01f6
ShivakumarRavi <shivakumar.mcet@gmail.com>
2020-02-17 00:01:32 +0530

    added html files

--- FileTransfer/client.py
+++ FileTransfer/client.py
@@ -1,24 +1,38 @@
 import socket
+from flask import Flask
+from flask import Flask, request, render_template
 
-TCP_IP = 'localhost'
-TCP_PORT = 9001
-BUFFER_SIZE = 1024
+app = Flask(__name__)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-with open('received_file', 'wb') as f:
-    print ('file opened')
-    while True:
-        #print('receiving data...')
-        data = s.recv(BUFFER_SIZE)
-        print('data=%s', (data))
-        if not data:
-            f.close()
-            print ('file close()')
-            break
-        # write data to a file
-        f.write(data)
+@app.route('/')
+def client():
+    return render_template('client.html')
 
-print('Successfully get the file')
-s.close()
-print('connection closed')
+#background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    TCP_IP = 'localhost'
+    TCP_PORT = 9001
+    BUFFER_SIZE = 1024
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    with open('received_file', 'wb') as f:
+        print ('file opened')
+        while True:
+            #print('receiving data...')
+            data = s.recv(BUFFER_SIZE)
+            print('data=%s', (data))
+            if not data:
+                f.close()
+                print ('file close()')
+                break
+            # write data to a file
+            f.write(data)
+
+    print('Successfully get the file')
+    s.close()
+    print('connection closed')
+    return {"status":200}
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1",port="8081")
--- FileTransfer/server.py
+++ FileTransfer/server.py
@@ -1,10 +1,13 @@
 import socket
 from threading import Thread
 from socketserver import ThreadingMixIn
+from flask import Flask, request, render_template
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
 BUFFER_SIZE = 1024
+
+app = Flask(__name__)
 
 class ClientThread(Thread):
 
@@ -29,19 +32,29 @@ class ClientThread(Thread):
                 self.sock.close()
                 break
 
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind((TCP_IP, TCP_PORT))
-threads = []
+@app.route('/')
+def server():
+    return render_template('server.html')
 
-while True:
-    tcpsock.listen(5)
-    print ("Waiting for incoming connections...")
-    (conn, (ip,port)) = tcpsock.accept()
-    print ('Got connection from ', (ip,port))
-    newthread = ClientThread(ip,port,conn)
-    newthread.start()
-    threads.append(newthread)
+#background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcpsock.bind((TCP_IP, TCP_PORT))
+    threads = []
 
-for t in threads:
-    t.join()
+    while True:
+        tcpsock.listen(5)
+        print ("Waiting for incoming connections...")
+        (conn, (ip,port)) = tcpsock.accept()
+        print ('Got connection from ', (ip,port))
+        newthread = ClientThread(ip,port,conn)
+        newthread.start()
+        threads.append(newthread)
+
+    for t in threads:
+        t.join()
+
+if __name__ == '__main__':
+    app.run()
